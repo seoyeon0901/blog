9cbba6fb057c68b3804b963e0133385f46e31a42
Seoyeon <99859378+seoyeon0901@users.noreply.github.com>
2023-03-03 15:45:12 +0900

    0303

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router');
+const posts = require('./posts/posts')
+
+
 const api = new Router();
 
-api.get('/test',ctx=>ctx.body='성공')
+api.use('/posts',posts.routes())
 
 module.exports = api;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
+//
 const Koa = require('koa');
 const Router = require('koa-router');
-const api = require('./api')
+const api = require('./api');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 //
 const app = new Koa();
 const router = new Router();
-
+const { PORT, MONGO_URI } = process.env;
 //
-
-router.use('/api', api.routes())
+mongoose.connect(MONGO_URI).then(() => {
+  console.log('mongodb 성공');
+}).catch(e => console.error(e));
+//
+app.use(bodyParser());
+router.use('/api', api.routes());
 //
 app.use(router.routes()).use(router.allowedMethods());
-app.listen(4000, () => {
+//
+const port = PORT || 4000;
+app.listen(port, () => {
   console.log('성공');
 });
